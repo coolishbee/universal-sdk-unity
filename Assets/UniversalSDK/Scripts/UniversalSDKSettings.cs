aa52748363a8dc05e92fb0857627143a88db5048
james-chun <chc3484@gmail.com>
2023-06-27 00:01:06 +0900

    [update] Modify intent flag singletask when calling Apple Login.

--- Assets/UniversalSDK/Scripts/UniversalSDKSettings.cs
+++ Assets/UniversalSDK/Scripts/UniversalSDKSettings.cs
@@ -8,7 +8,7 @@ namespace Universal.UniversalSDK
         public const string settingsAssetName = "UniversalSDKSettings";
         public const string settingsPath = "Editor/UniversalSDK";
         public const string settingsAssetExtension = ".asset";
-        public const string sdkVersion = "1.2.5";
+        public const string sdkVersion = "1.2.6";
 
         private static UniversalSDKSettings instance;
 
